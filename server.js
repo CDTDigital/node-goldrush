1ed1eac31d0256d9c1abeb9179f0dbad0f2c11af
Kane Baccigalupi <kane.baccigalupi@state.ca.gov>
2017-05-24 08:18:38 -0700

    Adds layout template delivered by home page

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 'use strict';
 
+const fs      = require('fs');
+
 const express = require('express');
 const server  = express();
 
+const layout  = fs.readFileSync(__dirname + '/server/templates/layout.html').toString();
+
 server.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.send(layout);
 });
 
 module.exports = server;
